cef2a2d924207a9fa9c28599ee27b9efd08d449d
Joshua Taylor <toasterrage@gmail.com>
2023-01-02 18:31:40 -0700

    Added modules and setting up server.js

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
-// Server JS
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
+const PORT = 3001;
+const app = express();
+
+
+// TODO - API route GET /api/notes
+// TODO - API route POST /api/notes
+// TODO - GET * should return index.html
+// TODO - GET /notes should return notes.html
+// TODO - Use FS to read/write to db.json
+
+
+app.listening(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+});
