82e35793756b3862495dd6320cf044dace3cf50f
Joshua Taylor <toasterrage@gmail.com>
2023-01-03 22:47:19 -0700

    Added framework for missing routes

--- server.js
+++ server.js
@@ -2,18 +2,42 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
+const db = require('./db/db.json');
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.json());
+//app.use('/api', api);
 
 app.use(express.static('public'));
 
-
+// GET route for homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+// GET route for notes page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// GET route for the notes request at /api/notes
+app.get('/api/notes', (req, res) => {
+    // Handle existing notes request
+});
+
+//POST route for the notes request at /api/notes
+app.post('/api/notes', (req, res) => {
+    // Handle saving notes request
+});
+
+// DELETE route for the notes request at /api/notes
+app.delete('/api/notes:id', (req, res) => {
+    // Handle note delete request
+});
+
+// TODO - Missing route handler
 
 // TODO - API route GET /api/notes
 // TODO - API route POST /api/notes
